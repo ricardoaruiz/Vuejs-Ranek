d9778870dc3932de8edb8fc901327e459f2cd5d2
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2019-08-04 11:53:24 -0300

    Criação de API fake (Json-server) e inicio da listagem de produtos

--- src/components/RnkListProduct.vue
+++ src/components/RnkListProduct.vue
@@ -1,12 +1,32 @@
 <template>
-  <div>
-    <p>Listagem de Produtos</p>
-  </div>
+  <section class="product_container">
+    <div v-for="product in products" :key="product.id">
+      <img :src="product.images[0].content" :alt="product.images[0].title" />
+      <p class="product_price">{{ product.price }}</p>
+      <h2 class="product_title">{{ product.name }}</h2>
+      <p class="product_description">{{ product.description }}</p>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-  name: "RnkListProduct"
+  name: "RnkListProduct",
+  data() {
+    return {
+      products: null
+    };
+  },
+  methods: {
+    getProducts() {
+      fetch("http://localhost:3000/product")
+        .then(result => result.json())
+        .then(products => (this.products = products));
+    }
+  },
+  created() {
+    this.getProducts();
+  }
 };
 </script>
 
