9cd9daf18bfdd41b18aa2cdb01e600e1019fbc9c
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2019-08-04 11:14:14 -0300

    Criação dos componentes de busca e listagem de produtos (inicial) e chamada na home

---
+++ src/components/RnkListProduct.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>Listagem de Produtos</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RnkListProduct"
+};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,27 @@
 <template>
-  <div></div>
+  <section>
+    <h1>Compre ou venda</h1>
+    <RnkFindProduct />
+    <RnkListProduct />
+  </section>
 </template>
 
 <script>
+import RnkFindProduct from "@/components/RnkFindProduct.vue";
+import RnkListProduct from "@/components/RnkListProduct.vue";
+
 export default {
-  name: "home",
-  components: {}
+  name: "Home",
+  components: { RnkFindProduct, RnkListProduct }
 };
 </script>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+  color: #87f;
+  margin-top: 40px;
+  text-align: center;
+}
+</style>
+
